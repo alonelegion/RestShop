75589f303e2b7d206fcea3c86278f1e70cda4c4f
AloneLegion <com.berdin@gmail.com>
2019-11-27 11:56:05 +0300

    mongodb and mongoose added commit

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productsRouts = require('./api/routes/products');
 const orderRouts = require('./api/routes/orders');
 
+mongoose.connect(
+    'mongodb+srv://admin:' + 
+    process.env.MONGO_ATLAS_PW + 
+    '@node-rest-shop-xkokr.mongodb.net/test?retryWrites=true&w=majority', 
+    {
+    useMongoClient: true
+});
+
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+/*
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({})
+    }
+});
+*/
 
 // Routes whitch shold handle requests
 app.use('/products', productsRouts);
